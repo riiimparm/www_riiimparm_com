4b9af19281699776065be111b17055a645deb144
riiimparm <riiimparm@gmail.com>
2025-11-23 20:36:22 +0900

    ✨ feat: add products

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,31 +1,40 @@
 // src/data/products.ts
 
 export interface ProductItem {
-  title: string;
-  url: string;
-  description: string;
-  image: string;
-  techStack: string[];
+    title: string;
+    url: string;
+    description: string;
+    image: string;
+    techStack: string[];
 }
 
 export const products: ProductItem[] = [
-  {
-    title: "Meeats BoardGame&Kitchen",
-    url: "https://meeats-bgk.com/",
-    description: `
+    {
+        title: "Meeats BoardGame&Kitchen",
+        url: "https://meeats-bgk.com/",
+        description: `
       My Friend's Restaurant's official website.
       Built using Next.js and Supabase.
     `,
-    image: "https://fnyfcmfyjoirlrsrdzva.supabase.co/storage/v1/object/public/img//thumbnail.jpg",
-    techStack: ["Next.js", "Supabase", "Tailwind CSS"]
-  },
-  {
-    title: "Burpee - BurpSuite Extension",
-    url: "https://github.com/riiimparm/burpee",
-    description: `
+        image: "https://fnyfcmfyjoirlrsrdzva.supabase.co/storage/v1/object/public/img//thumbnail.jpg",
+        techStack: ["Next.js", "Supabase", "Tailwind CSS"]
+    },
+    {
+        title: "Burpee - BurpSuite Extension",
+        url: "https://github.com/riiimparm/burpee",
+        description: `
       Burp Suite extension that organizes HTTP requests and exports them to Excel.
     `,
-    image: "/product/burpee.png",
-    techStack: ["Kotlin", "Burp Suite API"]
-  }
+        image: "/product/burpee.webp",
+        techStack: ["Kotlin", "Burp Suite API"]
+    },
+    {
+        title: "Scanned Notify - BurpSuite Extension",
+        url: "https://github.com/riiimparm/scanned_notify",
+        description: `
+      Notifies you via Gmail when a Task Scan is complete.
+    `,
+        image: "/product/scanned_notify.webp",
+        techStack: ["Kotlin", "Burp Suite API"]
+    }
 ];
